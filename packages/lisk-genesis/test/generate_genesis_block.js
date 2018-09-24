a1a5a73287771e9b25a8b700037d15e25eafae26
Shusetsu Toda <shusetsu@lightcurve.io>
2018-09-24 11:19:52 +0200

    :white_check_mark: Add transaction call test

--- packages/lisk-genesis/test/generate_genesis_block.js
+++ packages/lisk-genesis/test/generate_genesis_block.js
@@ -12,16 +12,18 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
-import cryptographyModule from 'lisk-cryptography';
-import passphraseModule from 'lisk-passphrase';
 import {
 	getBlockBytes,
 	getBlockId,
 	createAccount,
+	createTransferTx,
+	createSecondSignatureTx,
 } from '../src/generate_genesis_block';
 import defaultBlock from '../fixtures/block.json';
-
-const { Mnemonic } = passphraseModule;
+// Required for stubbing
+const cryptographyModule = require('lisk-cryptography');
+const transactionModule = require('lisk-transactions');
+const passphraseModule = require('lisk-passphrase');
 
 describe('Generate genesis block', () => {
 	const defaultBlockHash =
@@ -55,24 +57,122 @@ describe('Generate genesis block', () => {
 		};
 
 		beforeEach(() => {
-			sandbox.stub(Mnemonic, 'generateMnemonic').returns(defaultPassphrase);
+			sandbox
+				.stub(passphraseModule.default.Mnemonic, 'generateMnemonic')
+				.returns(defaultPassphrase);
 			return sandbox
-				.stub(cryptographyModule, 'getAddressAndPublicKeyFromPassphrase')
+				.stub(
+					cryptographyModule.default,
+					'getAddressAndPublicKeyFromPassphrase',
+				)
 				.withArgs(defaultPassphrase)
 				.returns(defaultKeys);
 		});
 
 		it('should create an account', () => {
 			const account = createAccount();
-			expect(Mnemonic.generateMnemonic).to.be.called;
+			expect(passphraseModule.default.Mnemonic.generateMnemonic).to.be.called;
 			expect(
-				cryptographyModule.getAddressAndPublicKeyFromPassphrase,
+				cryptographyModule.default.getAddressAndPublicKeyFromPassphrase,
 			).to.be.calledWithExactly(defaultPassphrase);
 			return expect(account).to.be.eql({
 				passphrase: defaultPassphrase,
 				address: defaultKeys.address,
 				publicKey: defaultKeys.publicKey,
 			});
+		});
+	});
+
+	describe('#createTransferTx', () => {
+		const defaultAmount = '100';
+		const defaultSenderId = '14389576228799148035L';
+		const defaultSenderPublicKey =
+			'88b182d9f2d8a7c3b481a8962ae7d445b7a118fbb6a6f3afcedf4e0e8c46ecac';
+		const defaultRecipientId = '10498496668550693658L';
+		const defaultRecipientPublicKey =
+			'90215077294ac1c727b357978df9291b77a8a700e6e42545dc0e6e5ba9582f13';
+		const defaultPassphrase =
+			'lab mirror fetch tuna village sell sphere truly excite manual planet capable';
+
+		beforeEach(() => {
+			transactionModule.default.utils = {
+				prepareTransaction: sandbox.stub(),
+			};
+			return Promise.resolve();
+		});
+
+		it('should call prepareTransaction with the correct input', () => {
+			const transactionInput = {
+				amount: defaultAmount,
+				senderId: defaultSenderId,
+				senderPublicKey: defaultSenderPublicKey,
+				recipientId: defaultRecipientId,
+				recipientPublicKey: defaultRecipientPublicKey,
+			};
+			createTransferTx({
+				...transactionInput,
+				passphrase: defaultPassphrase,
+			});
+			return expect(
+				transactionModule.default.utils.prepareTransaction,
+			).to.be.calledWithExactly(
+				{
+					type: 0,
+					fee: '0',
+					timestamp: 0,
+					...transactionInput,
+					asset: {},
+				},
+				defaultPassphrase,
+			);
+		});
+	});
+
+	describe('#createSecondSignatureTx', () => {
+		const defaultSenderId = '14389576228799148035L';
+		const defaultSenderPublicKey =
+			'88b182d9f2d8a7c3b481a8962ae7d445b7a118fbb6a6f3afcedf4e0e8c46ecac';
+		const defaultSecondPublicKey =
+			'90215077294ac1c727b357978df9291b77a8a700e6e42545dc0e6e5ba9582f13';
+		const defaultPassphrase =
+			'lab mirror fetch tuna village sell sphere truly excite manual planet capable';
+
+		beforeEach(() => {
+			transactionModule.default.utils = {
+				prepareTransaction: sandbox.stub(),
+			};
+			return Promise.resolve();
+		});
+
+		it('should call prepareTransaction with the correct input', () => {
+			const transactionInput = {
+				senderId: defaultSenderId,
+				senderPublicKey: defaultSenderPublicKey,
+			};
+			createSecondSignatureTx({
+				...transactionInput,
+				secondPublicKey: defaultSecondPublicKey,
+				passphrase: defaultPassphrase,
+			});
+			return expect(
+				transactionModule.default.utils.prepareTransaction,
+			).to.be.calledWithExactly(
+				{
+					type: 1,
+					fee: '0',
+					timestamp: 0,
+					amount: '0',
+					...transactionInput,
+					recipientId: defaultSenderId,
+					recipientPublicKey: defaultSenderPublicKey,
+					asset: {
+						signature: {
+							publicKey: defaultSecondPublicKey,
+						},
+					},
+				},
+				defaultPassphrase,
+			);
 		});
 	});
 });
