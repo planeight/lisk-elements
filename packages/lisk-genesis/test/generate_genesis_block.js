671c44a6801ac2b3c3a2a5d6566510993d8d7180
Shusetsu Toda <shusetsu@lightcurve.io>
2018-09-24 17:54:09 +0200

    :recycle: Update fixtures and add tests

--- packages/lisk-genesis/test/generate_genesis_block.js
+++ packages/lisk-genesis/test/generate_genesis_block.js
@@ -18,6 +18,9 @@ import {
 	createAccount,
 	createTransferTx,
 	createSecondSignatureTx,
+	createRegisterDelegateTx,
+	createVoteTx,
+	getTransactionPayloadInfo,
 } from '../src/generate_genesis_block';
 import defaultBlock from '../fixtures/block.json';
 // Required for stubbing
@@ -27,9 +30,9 @@ const passphraseModule = require('lisk-passphrase');
 
 describe('Generate genesis block', () => {
 	const defaultBlockHash =
-		'00000000c0b21900848f52d212fb9f261500000083c9d5ef000000008058840c000000000065cd1d00000000d90e000040aa7dc0114977bb3b8b04ec0a5a7a298212ce1fbbd15038e39f0ac4f53f118f4f12f49ee704d8554dc3dc392ba3ecf7a6e6f700b291fd77f75181cb48e34ad6';
+		'00000000c6ed1f04d75d75ef8fb87d31190000004e1a1a6c0000000080b2e60e000000000084d717000000006d0b000032993aa2513354be689acf6eb5dbc683653313126d727da489a3c84c0c9a4a0f2e334da068a75c4ee5e7d82092ef30f70209b24fd29a008c2cf753bec091b04a';
 	const defaultBlockHashWithSignature =
-		'00000000c0b21900848f52d212fb9f261500000083c9d5ef000000008058840c000000000065cd1d00000000d90e000040aa7dc0114977bb3b8b04ec0a5a7a298212ce1fbbd15038e39f0ac4f53f118f4f12f49ee704d8554dc3dc392ba3ecf7a6e6f700b291fd77f75181cb48e34ad6c9c8a9a0d0ba1c8ee519792f286d751071de588448eb984ddd9fe4ea0fe34db474692407004047068dee785abca22a744203fb0342b5404349fa9d6abab1480d';
+		'00000000c6ed1f04d75d75ef8fb87d31190000004e1a1a6c0000000080b2e60e000000000084d717000000006d0b000032993aa2513354be689acf6eb5dbc683653313126d727da489a3c84c0c9a4a0f2e334da068a75c4ee5e7d82092ef30f70209b24fd29a008c2cf753bec091b04a6f48350e72fca55965b1c1cbe401942a90ade42dbff1db116e93f1e3ae8593cbf10689a92ea9a01691590b6c561dd9241e2b9f1999ddf87afc07ac486dd51a06';
 
 	describe('#getBlockBytes', () => {
 		it('should have the correct block hash', () => {
@@ -96,21 +99,19 @@ describe('Generate genesis block', () => {
 
 		beforeEach(() => {
 			transactionModule.default.utils = {
+				...transactionModule.default.utils,
 				prepareTransaction: sandbox.stub(),
 			};
 			return Promise.resolve();
 		});
 
 		it('should call prepareTransaction with the correct input', () => {
-			const transactionInput = {
+			createTransferTx({
 				amount: defaultAmount,
 				senderId: defaultSenderId,
 				senderPublicKey: defaultSenderPublicKey,
 				recipientId: defaultRecipientId,
 				recipientPublicKey: defaultRecipientPublicKey,
-			};
-			createTransferTx({
-				...transactionInput,
 				passphrase: defaultPassphrase,
 			});
 			return expect(
@@ -120,7 +121,11 @@ describe('Generate genesis block', () => {
 					type: 0,
 					fee: '0',
 					timestamp: 0,
-					...transactionInput,
+					amount: defaultAmount,
+					senderId: defaultSenderId,
+					senderPublicKey: defaultSenderPublicKey,
+					recipientId: defaultRecipientId,
+					recipientPublicKey: defaultRecipientPublicKey,
 					asset: {},
 				},
 				defaultPassphrase,
@@ -139,18 +144,16 @@ describe('Generate genesis block', () => {
 
 		beforeEach(() => {
 			transactionModule.default.utils = {
+				...transactionModule.default.utils,
 				prepareTransaction: sandbox.stub(),
 			};
 			return Promise.resolve();
 		});
 
 		it('should call prepareTransaction with the correct input', () => {
-			const transactionInput = {
+			createSecondSignatureTx({
 				senderId: defaultSenderId,
 				senderPublicKey: defaultSenderPublicKey,
-			};
-			createSecondSignatureTx({
-				...transactionInput,
 				secondPublicKey: defaultSecondPublicKey,
 				passphrase: defaultPassphrase,
 			});
@@ -162,7 +165,8 @@ describe('Generate genesis block', () => {
 					fee: '0',
 					timestamp: 0,
 					amount: '0',
-					...transactionInput,
+					senderId: defaultSenderId,
+					senderPublicKey: defaultSenderPublicKey,
 					recipientId: defaultSenderId,
 					recipientPublicKey: defaultSenderPublicKey,
 					asset: {
@@ -173,6 +177,114 @@ describe('Generate genesis block', () => {
 				},
 				defaultPassphrase,
 			);
+		});
+	});
+
+	describe('#createRegisterDelegateTx', () => {
+		const defaultUsername = 'user1';
+		const defaultSenderId = '14389576228799148035L';
+		const defaultSenderPublicKey =
+			'88b182d9f2d8a7c3b481a8962ae7d445b7a118fbb6a6f3afcedf4e0e8c46ecac';
+		const defaultPassphrase =
+			'lab mirror fetch tuna village sell sphere truly excite manual planet capable';
+
+		beforeEach(() => {
+			transactionModule.default.utils = {
+				...transactionModule.default.utils,
+				prepareTransaction: sandbox.stub(),
+			};
+			return Promise.resolve();
+		});
+
+		it('should call prepareTransaction with the correct input', () => {
+			createRegisterDelegateTx({
+				username: defaultUsername,
+				senderId: defaultSenderId,
+				senderPublicKey: defaultSenderPublicKey,
+				passphrase: defaultPassphrase,
+			});
+			return expect(
+				transactionModule.default.utils.prepareTransaction,
+			).to.be.calledWithExactly(
+				{
+					type: 2,
+					fee: '0',
+					timestamp: 0,
+					amount: '0',
+					senderId: defaultSenderId,
+					senderPublicKey: defaultSenderPublicKey,
+					recipientId: defaultSenderId,
+					recipientPublicKey: defaultSenderPublicKey,
+					asset: {
+						delegate: {
+							username: defaultUsername,
+						},
+					},
+				},
+				defaultPassphrase,
+			);
+		});
+	});
+
+	describe('#createVoteTx', () => {
+		const defaultPublicKeys = [
+			'ee130982fccc345f4a8d89ca7764ddd946d317d5212a77c97ad9b4fa28e8b541',
+			'fa3d76794aeebf94da2a9c8e6aac2b323632bed7fe56fd85487cf6e09eff023a',
+		];
+		const defaultPublicKeysWithSign = [
+			'+ee130982fccc345f4a8d89ca7764ddd946d317d5212a77c97ad9b4fa28e8b541',
+			'+fa3d76794aeebf94da2a9c8e6aac2b323632bed7fe56fd85487cf6e09eff023a',
+		];
+		const defaultSenderId = '14389576228799148035L';
+		const defaultSenderPublicKey =
+			'88b182d9f2d8a7c3b481a8962ae7d445b7a118fbb6a6f3afcedf4e0e8c46ecac';
+		const defaultPassphrase =
+			'lab mirror fetch tuna village sell sphere truly excite manual planet capable';
+
+		beforeEach(() => {
+			transactionModule.default.utils = {
+				...transactionModule.default.utils,
+				prepareTransaction: sandbox.stub(),
+			};
+			return Promise.resolve();
+		});
+
+		it('should call prepareTransaction with the correct input', () => {
+			createVoteTx({
+				publicKeys: defaultPublicKeys,
+				senderId: defaultSenderId,
+				senderPublicKey: defaultSenderPublicKey,
+				passphrase: defaultPassphrase,
+			});
+			return expect(
+				transactionModule.default.utils.prepareTransaction,
+			).to.be.calledWithExactly(
+				{
+					type: 3,
+					fee: '0',
+					timestamp: 0,
+					amount: '0',
+					senderId: defaultSenderId,
+					senderPublicKey: defaultSenderPublicKey,
+					recipientId: defaultSenderId,
+					recipientPublicKey: defaultSenderPublicKey,
+					asset: {
+						votes: defaultPublicKeysWithSign,
+					},
+				},
+				defaultPassphrase,
+			);
+		});
+	});
+
+	describe('#getTransactionPayloadInfo', () => {
+		it('should result in the same payload hash and length of the fixture', () => {
+			return expect(
+				getTransactionPayloadInfo(defaultBlock.transactions),
+			).to.be.eql({
+				payloadHash: defaultBlock.payloadHash,
+				payloadLength: defaultBlock.payloadLength,
+			});
 		});
 	});
 });
