ddca207ad376393406c182725f16364cea81dab9
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-11-14 18:25:32 +0100

    WIP

---
+++ packages/lisk-p2p/src/index.ts
@@ -0,0 +1,11 @@
+// import socketClusterServer from 'socketcluster-server';
+// import socketClusterClient from 'socketcluster-client';
+
+function foo():void {
+  console.log(111);
+  // console.log(111, socketClusterServer.attach);
+}
+
+foo();
+
+export default {};
---
+++ packages/lisk-p2p/test/index.ts
@@ -0,0 +1,3 @@
+import P2P from '../src/index'
+
+console.log(123, P2P);
