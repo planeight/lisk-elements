ff555a0f9566d5e6fb5f024c924df283bcf8e490
Ishan <ishantiw.quasar@gmail.com>
2019-01-31 16:49:31 +0100

    :recycle: Remove Peer objects in Selection algo and use it in Peer Pool

--- packages/lisk-p2p/src/p2p.ts
+++ packages/lisk-p2p/src/p2p.ts
@@ -18,9 +18,7 @@ import * as http from 'http';
 import { attach, SCServer, SCServerSocket } from 'socketcluster-server';
 import * as url from 'url';
 
-import { RequestFailError } from './errors';
 import { Peer } from './peer';
-import { PeerOptions } from './peer_selection';
 
 import {
 	INVALID_CONNECTION_QUERY_CODE,
@@ -164,30 +162,13 @@ export class P2P extends EventEmitter {
 
 	// TODO ASAP: Change selectPeers to return PeerInfo list and then make request on peerPool itself; pass peerInfo as argument.
 	public async request(packet: P2PRequestPacket): Promise<P2PResponsePacket> {
-		const peerSelectionParams: PeerOptions = {
-			lastBlockHeight: this._nodeInfo.height,
-		};
-		const selectedPeer = this._peerPool.selectPeers(peerSelectionParams, 1);
-
-		if (selectedPeer.length <= 0) {
-			throw new RequestFailError(
-				'Request failed due to no peers found in peer selection',
-			);
-		}
-		const response: P2PResponsePacket = await selectedPeer[0].request(packet);
+		const response = await this._peerPool.requestPeer(packet, this._nodeInfo);
 
 		return response;
 	}
 
 	public send(message: P2PMessagePacket): void {
-		const peerSelectionParams: PeerOptions = {
-			lastBlockHeight: this._nodeInfo.height,
-		};
-		const selectedPeers = this._peerPool.selectPeers(peerSelectionParams);
-
-		selectedPeers.forEach((peer: Peer) => {
-			peer.send(message);
-		});
+		this._peerPool.sendToPeers(message, this._nodeInfo);
 	}
 
 	private async _startPeerServer(): Promise<void> {
